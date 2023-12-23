e00d8ab133497a7bf07f79949df49185747a290a
Movsar Bekaev <movsar.dev@gmail.com>
2023-12-23 11:53:54 +0300

    сделано # 11 и 12

--- CakesAdvanced/Models/Cake.cs
+++ CakesAdvanced/Models/Cake.cs
@@ -16,8 +16,15 @@ namespace CakesAdvanced.Models
         internal Cake(string name, List<Ingredient> ingredients)
         {
             Name = name;
-            _ingredients = ingredients; 
-        }
+            _ingredients = ingredients;
+            Price = _ingredients.Sum(i => (i.Cost * 0.5m) * i.Quantity);
 
+            //decimal price = 0;
+            //foreach (var ingredient in _ingredients)
+            //{
+            //   price = price + (ingredient.Cost * ingredient.Quantity);
+            //}
+            //Price = price * 0.5m;
+        }  
     }
 }
