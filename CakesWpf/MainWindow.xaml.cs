829463e7ce89c36869c94b4f74c74e345a4548dc
Муслим Марашев <appl95@bk.ru>
2024-01-10 19:22:01 +0300

    Закончил

---
+++ CakesWpf/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CakesLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CakesWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+        private Storage _storage;
+        private Kitchen _kitchen;
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            Ingredient ingredient;
+            _storage = new Storage();
+            _kitchen = new Kitchen(_storage);
+            var availableIngredients = _storage.GetAllIngredients();
+            Ingredients.AddRange(availableIngredients);
+            Storage storage = new Storage(); Kitchen kitchen = new Kitchen(storage);
+        }
+    }
+}
