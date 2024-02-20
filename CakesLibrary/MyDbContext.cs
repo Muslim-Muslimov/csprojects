2f27c855a96cf9a80d263da17bb0e2711d2bf4d6
Муслим Марашев <appl95@bk.ru>
2024-02-20 22:01:44 +0300

    1

---
+++ CakesLibrary/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CakesLibrary.Models;
+using  Microsoft.EntityFrameworkCore;
+
+namespace CakesLibrary
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Ingredient> _ingredients { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = $"Data Source=F:\\c# projects\\csprojects\\CakesLibrary";
+            optionsBuilder.UseSqlite(connectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- ConsoleUtils/Program.cs
+++ ConsoleUtils/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace ConsoleUtils
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
